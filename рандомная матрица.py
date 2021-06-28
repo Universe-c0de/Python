5484403fb115ed4273945241dd68b50d5e8e68ea
Universe-c0de <85509930+Universe-c0de@users.noreply.github.com>
2021-06-28 21:44:58 +0300

    Update рандомная матрица.py

--- рандомная матрица.py
+++ рандомная матрица.py
@@ -4,10 +4,10 @@ n = int(input("Введите значение n: "))
 m = int(input("Введите значение m: "))
 x = int(input("Максимальное значение рандомного числа: "))
 
-a = [[randint(0, x) for a in range(n)] for b in range(m)]
+matrix = [[randint(0, x) for a in range(n)] for b in range(m)]
 
 for i in range(m):
     for j in range(n):
-        print(a[i][j], end=' ')
+        print(matrix[i][j], end=' ')
     print("")
     
